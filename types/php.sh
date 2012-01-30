a532d3a6907396df15ea81a152c3a89e3934adc4
Cyril Adrian <cyril.adrian@gmail.com>
2012-01-30 18:12:54 +0100

    PHP project type

---
+++ types/php.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME=$1
+PROJECT=$PROJECTS_DIR/$1
+PROJECT_DEVDIR=$2
+
+. $PROJECT_PACK/bash_profile.sh
+
+
+make_emacs() {
+    cat > $PROJECT/project.el <<EOF
+(setq load-path (cons "$PROJECT_PACK/site-lisp" (cons "$PROJECT_PACK/site-lisp/mk-project" load-path)))
+
+(require 'mk-project)
+(global-set-key (kbd "C-c p c") 'project-compile)
+(global-set-key (kbd "C-c p l") 'project-load)
+(global-set-key (kbd "C-c p a") 'project-ack)
+(global-set-key (kbd "C-c p g") 'project-grep)
+(global-set-key (kbd "C-c p o") 'project-multi-occur)
+(global-set-key (kbd "C-c p u") 'project-unload)
+(global-set-key (kbd "C-c p f") 'project-find-file) ; or project-find-file-ido
+(global-set-key (kbd "C-c p i") 'project-index)
+(global-set-key (kbd "C-c p s") 'project-status)
+(global-set-key (kbd "C-c p h") 'project-home)
+(global-set-key (kbd "C-c p d") 'project-dired)
+(global-set-key (kbd "C-c p t") 'project-tags)
+
+(project-def "$PROJECT_NAME-project"
+      '((basedir          "$PROJECT_DEVDIR")
+        (src-patterns     ("*.e"))
+        (ignore-patterns  ("*.o"))
+        (tags-file        "$PROJECT/.mk/TAGS")
+        (file-list-cache  "$PROJECT/.mk/files")
+        (open-files-cache "$PROJECT/.mk/open-files")
+        (vcs              git)
+        (compile-cmd      nil)
+        (startup-hook     $PROJECT_NAME-project-startup)
+        (shutdown-hook    nil)))
+
+(defun $PROJECT_NAME-project-startup ()
+  nil)
+
+(set-frame-name "Emacs: $PROJECT_NAME")
+(project-load "$PROJECT_NAME-project")
+(project-dired)
+EOF
+}
+
+
+make_tags() {
+    cat > $PROJECT/bin/tag_all.sh <<EOF
+#!/usr/bin/env bash
+
+export PROJECT=\${PROJECT:-$PROJECT}
+export TAGS=\${TAGS:-\$PROJECT/.mk/TAGS}
+export PROJECT_DEVDIR=\$(readlink \$PROJECT/dev)
+etags \$@ -f \$TAGS --language-force=PHP \$(find \$PROJECT_DEVDIR -name \\*.php -o -name \\*.html) 2>/dev/null || echo "Brand new project: no file tagged."
+
+if [ -d \$PROJECT/dep ]; then
+    for dep in \$(echo \$PROJECT/dep/*); do
+        if [ -h \$dep ]; then
+            project=$PROJECTS_DIR/\${dep#\$PROJECT/dep/}
+            PROJECT=\$project \$project/bin/tag_all.sh -a \$@
+        fi
+    done
+fi
+EOF
+
+    cat > $PROJECT/bin/find_all.sh <<EOF
+#!/usr/bin/env bash
+
+export PROJECT=\${PROJECT:-$PROJECT}
+export TAGS=\${TAGS:-\$PROJECT/.mk/TAGS}
+export PROJECT_DEVDIR=\$(readlink \$PROJECT/dev)
+find \$PROJECT_DEVDIR -name \\*.php -o -name \\*.html 2>/dev/null
+
+if [ -d \$PROJECT/dep ]; then
+    for dep in \$(echo \$PROJECT/dep/*); do
+        if [ -h \$dep ]; then
+            project=$PROJECTS_DIR/\${dep#\$PROJECT/dep/}
+            PROJECT=\$project \$project/bin/find_all.sh -a \$@
+        fi
+    done
+fi
+EOF
+
+    chmod +x $PROJECT/bin/tag_all.sh $PROJECT/bin/find_all.sh
+    _project_tag_all $PROJECT
+}
+
+
+make_go() {
+    cat > $PROJECT/go <<EOF
+
+export PATH=\$({
+        echo $PROJECT/bin
+        find -L $PROJECT/dev/ -type d -name bin | sort
+        test -d $PROJECT/dep && find -L $PROJECT/dep -type d -name bin | sort
+    } | awk '{printf("%s:", \$0)}'
+    echo \$PROJECT_DEFAULT_PATH
+)
+
+test "\$1" == "-fast" || _project_tag_all $PROJECT
+EOF
+
+    chmod +x $PROJECT/go
+}
+
+
+test -d $PROJECT/.mk || mkdir $PROJECT/.mk
+make_emacs
+make_tags
+make_go
