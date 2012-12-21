5f6c451fbeb8fcff969f9fb7154128a0a3644a0f
Cyril Adrian <cyril.adrian@gmail.com>
2012-12-21 09:38:50 +0100

    my project is older than I thought

--- utils/modules_finder.py
+++ utils/modules_finder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2011-2012, Cyril Adrian <cyril.adrian@gmail.com> All rights reserved.
+# Copyright (c) 2010-2012, Cyril Adrian <cyril.adrian@gmail.com> All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 # the following conditions are met:
